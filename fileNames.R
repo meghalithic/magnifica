17ff8520873cf24ffdbb3740f3a957f9add39213
Meghan Balk <meghan.balk@gmail.com>
2023-03-27 13:42:02 +0200

    adding formation info and checking errors

--- fileNames.R
+++ fileNames.R
@@ -139,7 +139,10 @@ tots <- df.list %>%
 
 length(setdiff(tots$specimenNR, bryo.meta$SPECIMEN.NR)) #136 in df.list that are not in bryo.meta
 length(setdiff(bryo.meta$SPECIMEN.NR, tots$specimenNR)) #8 in bryo.meta that are not in df.list
-setdiff(bryo.meta$SPECIMEN.NR, tots$specimenNR) #e.g., bryo.meta has 204CV, df.list has 204CC
+
+## look at 8 in bryo.meta that are not in df.list
+setdiff(bryo.meta$SPECIMEN.NR, tots$specimenNR) 
+#e.g., bryo.meta has 204CV, df.list has 204CC
 #232CV in bryo.meta is 232CC in df.list
 #005CC in bryo.meta is 005CV in df.list
 #0367iCV does not exist in df.list
@@ -147,16 +150,6 @@ setdiff(bryo.meta$SPECIMEN.NR, tots$specimenNR) #e.g., bryo.meta has 204CV, df.l
 #466CV in bryo.meta is 466CC in df.list
 #568CC in bryo.meta is 568CV in df.list
 #717iCV does not exist in df.list, but 717CV does
-
-extra.df <- setdiff(tots$specimenNR, bryo.meta$SPECIMEN.NR)
-extra.bryo <- setdiff(bryo.meta$SPECIMEN.NR, tots$specimenNR)
-fromDataset <- c(rep("imageFiles", length(extra.df)), rep("metadata", length(extra.bryo)))
-imageDiff <- c(extra.df, extra.bryo)
-errors.df <- cbind(fromDataset, imageDiff)
-
-write.csv(errors.df,
-          "errors.csv",
-          row.names = FALSE)
 
 #NR OF PICS in bryo.meta don't match the total number of images taken
 max(bryo.meta$NR.OF.PICS) #4
@@ -166,6 +159,28 @@ df.list[df.list$specimenNR == "237CC",]
 #should be only 3 images
 #has 237_CC_2_15v_x40_BSE replicated
 #has 237_CC_1_15v_x30_BSE_BTK2
+
+## look at 136 in df.list that are not in bryo.meta
+setdiff(tots$specimenNR, bryo.meta$SPECIMEN.NR)
+
+
+extra.df <- setdiff(tots$specimenNR, bryo.meta$SPECIMEN.NR)
+extra.bryo <- setdiff(bryo.meta$SPECIMEN.NR, tots$specimenNR)
+
+df.trim <- df.list[df.list$specimenNR %in% extra.df &
+                   df.list$ext == "tif",]
+df.trimmed <- df.trim[!duplicated(df.trim[c('specimenNR')]), ]
+
+dates <- c(rep("NA", length(extra.df)), bryo.meta$DATE[bryo.meta$SPECIMEN.NR %in% extra.bryo])
+folder <- c(df.trimmed$folder, rep("NA", length(extra.bryo)))
+formation <- c(df.trimmed$formation, rep("NA", length(extra.bryo)))
+fromDataset <- c(rep("imageFiles", length(extra.df)), rep("metadata", length(extra.bryo)))
+imageDiff <- c(extra.df, extra.bryo)
+errors.df <- cbind(fromDataset, imageDiff, folder, formation, dates)
+
+write.csv(errors.df,
+          "errors.csv",
+          row.names = FALSE)
 
 ##### COMPARE FORMATION NAMES -----
 #don't have the same formations
