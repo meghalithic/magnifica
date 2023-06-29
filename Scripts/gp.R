09606a5fba735de0488b780d460329f3bccba812
Meghan Balk <meghan.balk@gmail.com>
2023-06-29 10:56:00 +0200

    cleaning up and new results

--- Scripts/gp.R
+++ Scripts/gp.R
@@ -1,7 +1,7 @@
 # Meghan A. Balk
 # meghan.balk@gmail.com
 # initially created: Jun 2023
-# last updated: 27 Jun 2023
+# last updated: 28 Jun 2023
 print("update 'last updated' & set working directory!")
 
 # The purpose of this script is to create a P and G matrix for 
@@ -67,20 +67,20 @@ df$formation <- factor(df$formation, levels = c("NKLS", "NKBS", "Tewkesbury",
 formation_list <- unique(df$formation)
 length(formation_list) #7
 
-traits = names(df[, c("zh", "mpw.b", "cw.m", "cw.d", 
-                      "ow.m", "oh", "c.side", "o.side")])
+df$ln.zh <- log(df$zh)
+df$ln.mpw.b <- log(df$mpw.b)
+df$ln.cw.m <- log(df$cw.m)
+df$ln.cw.d <- log(df$cw.d)
+df$ln.ow.m <- log(df$ow.m)
+df$ln.oh <- log(df$oh)
+df$ln.o.side <- log(df$o.side)
+df$ln.c.side <- log(df$c.side)
+
+traits = names(df[, c("ln.zh", "ln.mpw.b", "ln.cw.m", "ln.cw.d", 
+                      "ln.ow.m", "ln.oh", "ln.c.side", "ln.o.side")])
 
 df.trim <- df %>%
   dplyr::select(zooid.id, colony.id, formation, matches(traits))
-
-#df$zh <- log10(df$zh)
-#df$mpw.b <- log10(df$mpw.b)
-#df$cw.m <- log10(df$cw.m)
-#df$cw.d <- log10(df$cw.d)
-#df$ow.m <- log10(df$ow.m)
-#df$oh <- log10(df$oh)
-#df$o.side <- log10(df$o.side)
-#df$c.side <- log10(df$c.side)
 
 colNums <- match(c(traits, "zooid.id"), names(df.trim))
 #  4  6  7  8  9 10 12 13  1
@@ -91,7 +91,7 @@ df = as.data.frame(df.trim)
 Fig <- list ()
 for (i in 1:length(traits)){
   Fig[[i]] = ggplot(data = df)+ 
-    geom_density(aes(x = log10(df[,traits[i]]), 
+    geom_density(aes(x = df[,traits[i]], 
                      group = formation,
                      col = formation)) + 
     theme(legend.position = "none", text = element_text(size = 20)) +
@@ -117,14 +117,14 @@ head(dat_lg_N)
 mean_by_formation_colony = dat_lg_N %>% #use this going forward
   group_by(formation, colony.id) %>%
   summarize(n.zooid = length(unique(zooid.id)),
-            avg.zh = mean(zh, na.rm = T),
-            avg.mpw.b = mean(mpw.b, na.rm = T),
-            avg.cw.m = mean(cw.m, na.rm = T),
-            avg.cw.d = mean(cw.d, na.rm = T),
-            avg.ow.m = mean(ow.m, na.rm = T),
-            avg.o.side = mean(o.side, na.rm = T),
-            avg.c.side = mean(c.side, na.rm = T),
-            avg.oh = mean(oh, na.rm = T)) %>%
+            avg.zh = mean(ln.zh, na.rm = T),
+            avg.mpw.b = mean(ln.mpw.b, na.rm = T),
+            avg.cw.m = mean(ln.cw.m, na.rm = T),
+            avg.cw.d = mean(ln.cw.d, na.rm = T),
+            avg.ow.m = mean(ln.ow.m, na.rm = T),
+            avg.oh = mean(ln.oh, na.rm = T),
+            avg.o.side = mean(ln.o.side, na.rm = T),
+            avg.c.side = mean(ln.c.side, na.rm = T)) %>%
   as.data.frame()
 
 #means of means
@@ -136,22 +136,22 @@ mean_by_formation = mean_by_formation_colony %>%
             avg.cw.m = mean(avg.cw.m, na.rm = T),
             avg.cw.d = mean(avg.cw.d, na.rm = T),
             avg.ow.m = mean(avg.ow.m, na.rm = T),
+            avg.oh = mean(avg.oh, na.rm = T),
             avg.o.side = mean(avg.o.side, na.rm = T),
-            avg.c.side = mean(avg.c.side, na.rm = T),
-            avg.oh = mean(avg.oh, na.rm = T)) %>%
+            avg.c.side = mean(avg.c.side, na.rm = T)) %>%
   as.data.frame()
 
 colony_means = dat_lg_N %>%
   group_by(colony.id) %>%
-  summarize(n.zooid = length(unique(new.id)),
-            avg.zh = mean(zh, na.rm = T),
-            avg.mpw.b = mean(mpw.b, na.rm = T),
-            avg.cw.m = mean(cw.m, na.rm = T),
-            avg.cw.d = mean(cw.d, na.rm = T),
-            avg.ow.m = mean(ow.m, na.rm = T),
-            avg.o.side = mean(o.side, na.rm = T),
-            avg.c.side = mean(c.side, na.rm = T),
-            avg.oh = mean(oh, na.rm = T)) %>%
+  summarize(n.zooid = length(unique(zooid.id)),
+            avg.zh = mean(ln.zh, na.rm = T),
+            avg.mpw.b = mean(ln.mpw.b, na.rm = T),
+            avg.cw.m = mean(ln.cw.m, na.rm = T),
+            avg.cw.d = mean(ln.cw.d, na.rm = T),
+            avg.ow.m = mean(ln.ow.m, na.rm = T),
+            avg.oh = mean(ln.oh, na.rm = T),
+            avg.o.side = mean(ln.o.side, na.rm = T),
+            avg.c.side = mean(ln.c.side, na.rm = T)) %>%
   as.data.frame()
 
 means = dat_lg_N %>%
@@ -165,73 +165,77 @@ means = dat_lg_N %>%
             avg.oh = mean(oh, na.rm = T)) %>%
   as.data.frame()
 
-#### SCALE DATA ----
-dat_lda = mean_by_formation_colony #dat_lg_N
-dat_lda[, 4:11] = scale(dat_lda[, 4:11], center = F, scale = T) #just traits
-data_discriminant = dat_lda
-
-data_plot = na.omit(data_discriminant[, 1:11]) #all rows
-
-r3 <- lda(formula = formation ~ ., 
-          data = data_plot[, c(1, 4:11)], method = 'mle') #just traits + formation 
-
-plda = predict(object = r3, # predictions
-               newdata = data_plot)
-#proportion of the variance explained by each LD axis:
-prop.lda = r3$svd^2/sum(r3$svd^2) 
-
-dataset = data.frame(formation = (data_plot)[,"formation"],
-                     lda = plda$x)
-
-p1 <- ggplot(dataset) + 
-  geom_point(aes(lda.LD1, lda.LD2, color = formation), 
-             size = 1, alpha = .75) + 
-  labs(x = paste("LD1 (", percent(prop.lda[1]), ")", sep = ""),
-       y = paste("LD2 (", percent(prop.lda[2]), ")", sep = ""))
-p1
-ggsave(p1, file = "./Results/trait_discriminant_27Jun2023.png", 
-       width = 14, height = 10, units = "cm")
+## DISCRIMINANT ANALYSIS --> not needed now
+# dat_lda = mean_by_formation_colony #dat_lg_N
+# dat_lda[, 4:11] = scale(dat_lda[, 4:11], center = F, scale = T) #just traits
+# data_discriminant = dat_lda
+# 
+# data_plot = na.omit(data_discriminant[, 1:11]) #all rows
+# 
+# r3 <- lda(formula = formation ~ ., 
+#           data = data_plot[, c(1, 4:11)], method = 'mle') #just traits + formation 
+# 
+# plda = predict(object = r3, # predictions
+#                newdata = data_plot)
+# #proportion of the variance explained by each LD axis:
+# prop.lda = r3$svd^2/sum(r3$svd^2) 
+# 
+# dataset = data.frame(formation = (data_plot)[,"formation"],
+#                      lda = plda$x)
+# 
+# p1 <- ggplot(dataset) + 
+#   geom_point(aes(lda.LD1, lda.LD2, color = formation), 
+#              size = 1, alpha = .75) + 
+#   labs(x = paste("LD1 (", percent(prop.lda[1]), ")", sep = ""),
+#        y = paste("LD2 (", percent(prop.lda[2]), ")", sep = ""))
+# p1
+# ggsave(p1, file = "./Results/trait_discriminant_27Jun2023.png", 
+#        width = 14, height = 10, units = "cm")
 
 #### P MATRIX ----
-phen.var = lapply(form_data, function (x){ (cov(x[, 4:11]))}) #traits per colony (not variation within colony)
-
-#### G MATRIX ----
-#Preparing the data
 #check number of colonies NOT zooids:
 col_form = split.data.frame(mean_by_formation_colony, mean_by_formation_colony$formation) #zooids per formation
-sample_sizes = lapply(col_form, function(x){dim(x)[1]})
-#enough = as.matrix(sample_sizes) > 100 #more than 100 zooids per formation
-#form_final = col_form[names(enough[enough == T,])]
-
-by_form = split.data.frame(dat_lg_N, dat_lg_N$formation)
+#just to look; max 328, smallest 19
+col_form.n = lapply(col_form, function(x){dim(x)[1]})
+by_form = split.data.frame(mean_by_formation_colony, mean_by_formation_colony$formation)
 form_data = lapply(by_form, function(x) x[complete.cases(x),])
+p.cov = lapply(form_data, function (x){ (cov(x[, 4:11]))}) #traits per colony (not variation within colony)
+
+#### G MATRIX ----
+#keep at zooid level because correct for this later
+zoo_form = split.data.frame(dat_lg_N, dat_lg_N$formation) #zooids per formation
+#just to look; highest 7836, smallest 454
+zoo_form.n = lapply(zoo_form, function(x){dim(x)[1]})
+zooid_by_form = split.data.frame(dat_lg_N, dat_lg_N$formation)
+zooid_form_data = lapply(zooid_by_form, function(x) x[complete.cases(x),])
 
 ##### PRIORS -----
-phen.var = lapply(form_data, function (x){ (cov(x[, 4:11]))}) #traits of ALL; correct for colony later
+phen.var = lapply(zooid_form_data, function (x){ (cov(x[, 4:11]))}) #traits of ALL; correct for colony later
 prior = lapply(phen.var, function (x){list(G = list(G1 = list(V = x/2, nu = 2)),
                                            R = list(V = x/4, nu = 2))})
+
 
 ##### MCMC -----
 #Running the MCMC chain
 model_G = list()
-for (i in 1:1){ #length 7 because 7 formations
-  model_G[[i]] <- MCMCglmm(cbind(zh, mpw.b, cw.m, cw.d, #same order as in priors
-                                 ow.m, c.side, o.side, oh) ~ trait-1,
+for (i in 1:length(formation_list)){ #length 7 because 7 formations
+  model_G[[i]] <- MCMCglmm(cbind(ln.zh, ln.mpw.b, ln.cw.m, ln.cw.d, #same order as in priors
+                                 ln.ow.m, ln.c.side, ln.o.side, ln.oh) ~ trait-1,
                          #account for variation w/in colony:
                          random = ~us(trait):colony.id, #the number of these determines # of Gs #+ us(trait):formation
                          rcov = ~us(trait):units,
                          family = rep("gaussian", 8), #num of traits
-                         data = form_data[[i]],
+                         data = zooid_form_data[[i]],
                          nitt = 1500000, thin = 1000, burnin = 500000,
                          prior = prior[[i]], verbose = TRUE)
   
 }
 
-data.list = list(model_G, dat_lg_N, form_data, mean_by_formation_colony)
+data.list = list(model_G, dat_lg_N, zooid_form_data, mean_by_formation_colony)
 
-save(model_G, file = "./Results/g_matrices_data_1form.RData")
+save(data.list, file = "./Results/g_matrices_data_form.RData")
 
-load(file = "New_g_matrices.RData") #load the g matrices calculated above 
+#load(file = "./Results/g_matrices_data_form.RData") #load the g matrices calculated above 
 
 summary(model_G[[1]])
 summary(model_G[[2]])
@@ -325,10 +329,14 @@ ggsave(PC_dist, file = "./Results/PC_dist_form.png",
 ###### G NOISE ------
 ##Controlling for noise
 #Extend G
-G_ext = lapply(G_std, function (x){ ExtendMatrix(x, ret.dim = 6)$ExtMat}) #not 8 because last eigen value (#8) was negative and second to last (#7 was zero basically), so want to ignore it 
+G_ext = lapply(G_std, function (x){ ExtendMatrix(x, ret.dim = 7)$ExtMat}) #not 8 because last eigen value (#8) was negative
+#ignore warning from above
 lapply(G_ext, isSymmetric)  
 Ext_std_variances = lapply(G_ext, diag)
 Ext_eig_variances = lapply(G_ext, function (x) {eigen(x)$values})
+##need to create random cov.m for comparison
+#cov.m <- RandomMatrix(8, 1, 1, 100) 
+#G_list <- list(G_ext$NKLS, cov.m)
 comp_mat = RandomSkewers(G_ext) #need at least
 corr_mat = comp_mat$correlations + t(comp_mat$correlations) 
 diag(corr_mat) = 1
@@ -340,40 +348,81 @@ corrplot.mixed(corr_mat,upper = "number", lower = "pie")
 #How much difference in matrix structure is induced by sampling error?
 
 #Rarefaction of G
+##Rarefaction - How much difference in matrix structure is induced by sampling error?
 
-pop.size = c(5:500) #Starting at 500 individuals and going down to 5
-#is.positive.definite(G_ext[[1]]) #No
-#is.positive.definite(round(G_ext[[1]], 10))
-#cov.matrix = RandomMatrix(8, 1, 1, 100) #for testing
-#G_ext = list(G_ext$NKLS, cov.matrix) #for testing
-pop.dist <- lapply(pop.size, function (x){mvrnorm(n = x, rep(0, 8), 
-                                                  G_ext[[1]])})
-pop.cv <- lapply(pop.dist, cov)
-RS_result = RandomSkewers(pop.cv, G_ext[[1]])
-results.RS <- Rarefaction(mean_by_colony[, 2:9], RandomSkewers, num.reps = 20) # already logged
-res_melt = as.data.frame(data.matrix(melt(results.RS))) #melted dataframe
+# We do a rarefaction analyses and compare a 'true' matrix with an 
+# under-sampled version of itself and see whether the dissimilarities observed 
+# across species can be explained by sampling alone.
 
-#colonies per formation
-colony_samples = split.data.frame(col_means, col_means$specimenNR) #Sample size
+# Computing a pooled P across all individuals in all samples
+#P_pooled<-var(cbind(indata$tpg, indata$ds2, indata$lpt, indata$ds3), na.rm=TRUE)
+#G_test <- G_ext$NKLS
+#trait_means_global<-c(mean(indata$tpg, na.rm=TRUE), 
+#                      mean(indata$ds2, na.rm=TRUE), 
+#                      mean(indata$lpt, na.rm=TRUE), 
+#                      mean(indata$ds3, na.rm=TRUE))
+#P_pooled_mean_std <- meanStdG(P_pooled, trait_means_global)
+
+# We start by generating "individuals" (i.e., colonies) 
+# in a population with VCV patterns corresponding to a known G. 
+# We then calculate VCV matrices for those individuals and obtain their 
+# similarity with the 'true' G.
+repititions <- 200
+#Defining the population size. Each population size is repeated 200 times. 
+#Starting at 2 individuals and going up to 100, 
+col_form.n # max 328, min 19
+pop.size = sort(rep(c(2:500), repititions)) #make 500 so much bigger than max no. colonies
+pop.dist <- lapply(pop.size, function (x){mvrnorm(n = x, 
+                                                  rep(0,8), # mean 0 for 8 traits 
+                                                  G_ext[[1]])}) # Simulate individual trait data based on the first VCOV matrix in our time series and different sample sizes. 
+# Calculate VCOV matrices 
+pop.cv <- lapply(pop.dist, cov) 
+# Estimate selection gradients based on Random Skewers, comparing our true 
+# G matrix and the undersampled pooled P matrix. 
+RS_result = RandomSkewers(pop.cv, 
+                          G_ext[[1]]) 
+out_results <- cbind(RS_result$correlation, pop.size)
+
+# Computing Random Skewers between pairs of actual G matrices in our data
+#(n = 7 for 7 formations) 
+# sub-setting so that we only analyze the traits and formation of interest
+mean.df.sub <- mean_by_formation_colony[, c(1, 4:11)] 
+#remove rows with NA so that the sample size gets correct when we plot 
+#the sample size for the VCOV.
+mean.df_complete_cases <- mean.df.sub[complete.cases(mean.df.sub), ] #remove rows with NA so that the sample size gets correct when we plot the sample size for the VCOV.
+
+# Calculating the sample size for each time point
+colony_samples = split.data.frame(mean.df_complete_cases, mean.df_complete_cases$formation) #Sample size
 sample_sizes_G = lapply(colony_samples, function(x){dim(x)[1]})
-sample_sp = unlist(sample_sizes_G[sample_sizes_G > 28]) #more than 28 samples per thing (e.g., 28 colonies per species)
-comp_sampleN=matrix(0,6,6) #calculating the smallest sample 
 
-for (i in 1:length(sample_sp)){
-  for (j in 1:length(sample_sp)){
-    comp_sampleN[i,j]=min(sample_sp[i],sample_sp[j])
+comp_sampleN = matrix(0, 7, 7) #calculating the smallest sample size in the comparison among pairs of G 
+
+for (i in 1:length(sample_sizes_G)){
+  for (j in 1:length(sample_sizes_G)){
+    comp_sampleN[i,j]=min(as.numeric(sample_sizes_G[i]),as.numeric(sample_sizes_G[j]))
   }
-  
 }
-comp_sampleN=comp_sampleN[-4,-4]
-comp_mat=RandomSkewers(G_ext)
-melt_comp=melt(comp_mat$correlations[lower.tri(comp_mat$correlations)])
-melt_samples=melt(comp_sampleN[lower.tri(comp_sampleN)])
-obs_melt=cbind(melt_comp,melt_samples)
-colnames(obs_melt)=c("RS","N")
-ggplot(data=res_melt,aes(res_melt$L1,res_melt$value^2))+geom_point()+
-  geom_point(data=obs_melt,aes(obs_melt$N,obs_melt$RS), col="red")+
-  xlab("Sample size")+ ylab("Similarity")
+
+# Here, we do the actual Random Skewers test and 
+# combined the results (correlations in the respons to selection) 
+# with the smallest samle size for the pairs of G that are investigated
+comp_mat = RandomSkewers(G_ext)
+melt_comp = melt(comp_mat$correlations[lower.tri(comp_mat$correlations)])
+melt_samples = melt(comp_sampleN[lower.tri(comp_sampleN)])
+obs_melt = cbind(melt_comp, melt_samples)
+colnames(obs_melt) = c("RS","N")
+
+#Plotting the result
+plot(out_results[, 2], out_results[, 1], 
+       xlab = "Sample size", ylab = "Similarity", 
+       pch = 19, col = "grey")
+points(obs_melt$N, obs_melt$RS, 
+       col = "purple", pch = 19)
+
+
+
+
+
 
 # G-based neutrality test
 ##Cladogenetic
